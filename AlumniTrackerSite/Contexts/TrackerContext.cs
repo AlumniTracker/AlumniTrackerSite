76a780da7d760617785a0f30e9423a5159dcd682
Mitch Partee <mitch.partee@student.centralia.edu>
2022-05-24 11:15:18 -0700

    UTD 5/24/22

--- AlumniTrackerSite/Contexts/TrackerContext.cs
+++ AlumniTrackerSite/Contexts/TrackerContext.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using AlumniTrackerSite.Models;
 using AlumniTrackerSite.Data;
-using AlumniTrackerSite.Areas.Identity;
 using Microsoft.AspNetCore.Identity;
 
 namespace AlumniTrackerSite.Contexts
@@ -13,14 +12,20 @@ namespace AlumniTrackerSite.Contexts
     {
         public static IConfiguration Configuration;
 
-
         public TrackerContext(DbContextOptions<TrackerContext> options, IConfiguration configuration)
             : base(options)
         {
             Configuration = configuration;
         }
 
+        public virtual DbSet<AdminUser> AdminUsers { get; set; } = null!;
         public virtual DbSet<AlumniUser> AlumniUsers { get; set; } = null!;
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; } = null!;
+        public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; } = null!;
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; } = null!;
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; } = null!;
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; } = null!;
+        public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; } = null!;
         public virtual DbSet<IdentityUser> IdentityUsers { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -34,18 +39,32 @@ namespace AlumniTrackerSite.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<AdminUser>(entity =>
+            {
+                entity.HasKey(e => e.AdminId);
+
+                entity.ToTable("AdminUser");
+
+                entity.Property(e => e.AdminName).HasMaxLength(50);
+
+                entity.Property(e => e.AdminType).HasMaxLength(50);
+
+                entity.Property(e => e.Id).HasMaxLength(450);
+
+                entity.HasOne(d => d.IdNavigation)
+                    .WithMany(p => p.AdminUsers)
+                    .HasForeignKey(d => d.Id)
+                    .HasConstraintName("FK_AdminUser_AspNetUsers1");
+            });
+
             modelBuilder.Entity<AlumniUser>(entity =>
             {
-                entity.HasKey(e => e.StudentId)
+                entity.HasKey(e => e.AlumniId)
                     .HasName("PK__AlumniUs__1788CC4CE62D00D5");
 
                 entity.ToTable("AlumniUser");
 
-                entity.Property(e => e.StudentId).ValueGeneratedNever();
-
                 entity.Property(e => e.Address).HasMaxLength(100);
-
-                entity.Property(e => e.AdminType).HasMaxLength(150);
 
                 entity.Property(e => e.City).HasMaxLength(50);
 
@@ -56,6 +75,8 @@ namespace AlumniTrackerSite.Contexts
                 entity.Property(e => e.EmployerName).HasMaxLength(100);
 
                 entity.Property(e => e.FieldofEmployment).HasMaxLength(50);
+
+                entity.Property(e => e.Id).HasMaxLength(450);
 
                 entity.Property(e => e.Name).HasMaxLength(50);
 
@@ -73,12 +94,99 @@ namespace AlumniTrackerSite.Contexts
 
                 entity.Property(e => e.Zip).HasMaxLength(10);
 
-                //entity.Property(e => e.Id).HasMaxLength(450);
-                //entity.HasOne(d => d.User.Id)
-                //    .WithOne(d => d.)
-                //    .HasForeignKey(d => d.User.Id)
-                //    .OnDelete(DeleteBehavior.ClientSetNull)
-                //    .HasContraintName("fk_AlumniUser_AspNetUser");
+                entity.HasOne(d => d.IdNavigation)
+                    .WithMany(p => p.AlumniUsers)
+                    .HasForeignKey(d => d.Id)
+                    .HasConstraintName("FK_AlumniUser_AspNetUsers1");
+            });
+
+            modelBuilder.Entity<AspNetRole>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedName, "RoleNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedName] IS NOT NULL)");
+
+                entity.Property(e => e.Name).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<AspNetRoleClaim>(entity =>
+            {
+                entity.HasIndex(e => e.RoleId, "IX_AspNetRoleClaims_RoleId");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.AspNetRoleClaims)
+                    .HasForeignKey(d => d.RoleId);
+            });
+
+            modelBuilder.Entity<AspNetUser>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+                entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+                entity.Property(e => e.Email).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+
+                entity.Property(e => e.UserName).HasMaxLength(256);
+
+                entity.HasMany(d => d.Roles)
+                    .WithMany(p => p.Users)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "AspNetUserRole",
+                        l => l.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
+                        r => r.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
+                        j =>
+                        {
+                            j.HasKey("UserId", "RoleId");
+
+                            j.ToTable("AspNetUserRoles");
+
+                            j.HasIndex(new[] { "RoleId" }, "IX_AspNetUserRoles_RoleId");
+                        });
+            });
+
+            modelBuilder.Entity<AspNetUserClaim>(entity =>
+            {
+                entity.HasIndex(e => e.UserId, "IX_AspNetUserClaims_UserId");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserClaims)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserLogin>(entity =>
+            {
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+                entity.HasIndex(e => e.UserId, "IX_AspNetUserLogins_UserId");
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.ProviderKey).HasMaxLength(128);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserLogins)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserToken>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.Name).HasMaxLength(128);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserTokens)
+                    .HasForeignKey(d => d.UserId);
             });
 
             OnModelCreatingPartial(modelBuilder);
--- AlumniTrackerSite/Models/AlumniUser.cs
+++ AlumniTrackerSite/Models/AlumniUser.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using AlumniTrackerSite.Areas.Identity;
-using Microsoft.AspNetCore.Identity;
 
 namespace AlumniTrackerSite.Models
 {
     public partial class AlumniUser
     {
-        [Key]
-        [Required]
         public string StudentId { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string? EmployerName { get; set; }
@@ -17,18 +12,15 @@ namespace AlumniTrackerSite.Models
         public string? YearGraduated { get; set; }
         public string? Degree { get; set; }
         public string? Notes { get; set; }
-        public string? AdminType { get; set; }
         public DateTime? DateModified { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? Zip { get; set; }
         public string? Phone { get; set; }
-        public bool IsAdmin { get; set; }
+        public int AlumniId { get; set; }
+        public string? Id { get; set; }
 
-        //public string? Id { get; set; }
-        //public virtual IdentityUser? User { get; set; }
-
-
+        public virtual AspNetUser? IdNavigation { get; set; }
     }
 }
