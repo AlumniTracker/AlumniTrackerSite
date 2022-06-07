298708802eb741ead801a848743bdcc73025ecbc
CopperBanana <aiden.lundeen@student.centralia.edu>
2022-06-07 01:22:51 -0700

    EmailFile should now work, just needs to talk to identity

--- AlumniTrackerSite/Data/Email.cs
+++ AlumniTrackerSite/Data/Email.cs
@@ -1,54 +1,88 @@
 ﻿using MimeKit;
 using MailKit.Net.Smtp;
+using Newtonsoft.Json;
+//using System.Text.Json;
 
 namespace AlumniTrackerSite.Data
 {
-    public class Email
+    public static class Email
     {
+        private static EmailData _data;
+        private static SmtpClient _client;
+        public static EmailData Initialize()
+        {
+            string path = (System.IO.Directory.GetCurrentDirectory() + @"\data\Settings.json");
+            _client = new SmtpClient();
+            return JsonConvert.DeserializeObject<EmailData>(File.ReadAllText(path));
+        }
+        public static async Task SendConfirmMessage(string reciever)
+        {
+            if (_data == null)
+            { _data = Initialize(); }
+            await SendMessage(reciever,_data.ConfirmSubject,_data.ConfirmBody);
+        }
+        public static async Task SendResetPasswordMessage(string reciever)
+        {
+            if (_data == null)
+            { _data = Initialize(); }
+            await SendMessage(reciever, _data.ResetSubject, _data.ResetBody);
+        }
+        //public static void AdminSendEmail(List<string> Recievers)
+        //{
 
-        public static void SendMessage() {
+        //}
+        private static async Task SendMessage(string reciever, string subject, string body)
+        {
+            if(_data == null)
+            { _data = Initialize(); }
 
             MimeMessage message = new MimeMessage();
 
-            message.From.Add(new MailboxAddress("AppTest", "hate4g@gmail.com"));
+            message.From.Add(new MailboxAddress("AppTest", _data.AccountName));
 
-            message.To.Add(MailboxAddress.Parse("hate4g@gmail.com"));
+            message.To.Add(MailboxAddress.Parse(reciever));
 
-            message.Subject = "What are you gunna do? Cry??";
+            message.Subject = subject;
 
             message.Body = new TextPart("plain")
             {
-                Text = @"Yup. Cry about it, Beaoch"
+                Text = body
             };
-
-            Console.Write("Email: ");
-            string emailAddress = Console.ReadLine();
-
-            //pgkjlkmeqblofudf
-            Console.Write("Password: ");
-            string pass = Console.ReadLine();
-
-            SmtpClient client = new SmtpClient();
 
             try
             {
-                client.Connect("smtp.gmail.com", 465, true);
-                client.Authenticate(emailAddress, pass);
-                client.Send(message);
-
+                _client.Connect("smtp.gmail.com", 465, true);
+                _client.Authenticate(_data.AccountName, _data.AccountPass);
+                await _client.SendAsync(message);
                 Console.WriteLine("Email Sent!!");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            finally
-            {
-                client.Disconnect(true);
-                client.Dispose();
-            }
 
         }
 
+        public static void Dispose()
+        {
+            _client.Disconnect(true);
+            _client.Dispose();
+        }
+
+    }
+
+
+
+    public class EmailData
+    {
+        public string ConfirmSubject { get; set; }
+        public string ConfirmBody { get; set; }
+        public string ResetSubject { get; set; }
+        public string ResetBody { get; set; }
+        public string Html { get; set; }
+        public string AccountName { get; set; }
+        public string AccountPass { get; set; }
     }
 }
+
+
