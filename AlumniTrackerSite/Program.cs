5f48c5c214b8520ca2adf05af50a77c94b6c8c7f
Mitch Partee <mitch.partee@student.centralia.edu>
2022-06-14 12:57:09 -0700

    Added Login Restriction from Header. Deleted old file. Fixed my version of the login page. Formed login page better.

--- AlumniTrackerSite/Program.cs
+++ AlumniTrackerSite/Program.cs
@@ -11,10 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Conn") ?? throw new InvalidOperationException("Connection string 'Conn' not found.");
 
 builder.Services.AddDbContext<AlumniIdentityContext>(options =>
-    options.UseSqlServer(connectionString));;
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AlumniIdentityContext>();;
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AlumniIdentityContext>();
 
 // Add services to the container.
 builder.Services.AddDbContext<TrackerContext>();
