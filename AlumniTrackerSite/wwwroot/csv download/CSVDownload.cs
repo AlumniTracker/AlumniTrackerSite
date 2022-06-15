c67d6bb9105913119f0cc4e8bf52765d2f28675c
YuyaSum <hate4g@gmail.com>
2022-06-15 13:44:24 -0700

    Creating Scaffold of CSV

---
+++ AlumniTrackerSite/wwwroot/csv download/CSVDownload.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.IO;
+
+namespace AlumniTrackerSite.wwwroot.csv_download
+{
+    public class CSVDownload
+    {
+        
+
+        public void MakeCSV(string filepath) 
+        { 
+
+            StringBuilder sb = new StringBuilder();
+
+            // Add Information To A Thingy
+            csvfile.AppendLine("Killer,App,Death,SlayQueen");
+            csvfile.AppendLine("1,2,3,4");
+
+            File.AppendAllText(filepath, sb.ToString());
+
+        }
+    }
+}
